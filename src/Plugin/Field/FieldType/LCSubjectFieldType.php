4fa19ce871b6dba7d738db120ee23d494c020594
Alexander O'Neill <aoneill@upei.ca>
2019-04-15 13:25:10 -0300

    Initial commit

---
+++ src/Plugin/Field/FieldType/LCSubjectFieldType.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace Drupal\lc_subject_field\Plugin\Field\FieldType;
+
+use Drupal\Component\Utility\Random;
+use Drupal\Core\Field\FieldDefinitionInterface;
+use Drupal\Core\Field\FieldItemBase;
+use Drupal\Core\Field\FieldStorageDefinitionInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\Core\TypedData\DataDefinition;
+
+/**
+ * Plugin implementation of the 'lcsubject_field_type' field type.
+ *
+ * @FieldType(
+ *   id = "lcsubject_field_type",
+ *   label = @Translation("LC Subject Field Type"),
+ *   description = @Translation("Field for storing Library of Congress subject fields with lookup"),
+ *   default_widget = "lcsubject_widget_type",
+ *   default_formatter = "lcsubject_formatter_type"
+ * )
+ */
+class LCSubjectFieldType extends FieldItemBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultStorageSettings() {
+    return [
+      'max_length' => 255,
+      'is_ascii' => FALSE,
+      'case_sensitive' => FALSE,
+    ] + parent::defaultStorageSettings();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function propertyDefinitions(FieldStorageDefinitionInterface $field_definition) {
+    // Prevent early t() calls by using the TranslatableMarkup.
+    $properties['value'] = DataDefinition::create('string')
+      ->setLabel(new TranslatableMarkup('Text value'))
+      ->setSetting('case_sensitive', $field_definition->getSetting('case_sensitive'))
+      ->setRequired(TRUE);
+    $properties['url'] = DataDefinition::create('string')
+      ->setLabel('Subject URL')
+      ->setRequired(FALSE);
+    return $properties;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function schema(FieldStorageDefinitionInterface $field_definition) {
+    $schema = [
+      'columns' => [
+        'value' => [
+          'type' => $field_definition->getSetting('is_ascii') === TRUE ? 'varchar_ascii' : 'varchar',
+          'length' => (int) $field_definition->getSetting('max_length'),
+          'binary' => $field_definition->getSetting('case_sensitive'),
+        ],
+        'url' => [
+          'type' => 'varchar',
+          'length' => 512,
+          'binary' => FALSE,
+        ],
+      ],
+    ];
+
+    return $schema;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getConstraints() {
+    $constraints = parent::getConstraints();
+
+    if ($max_length = $this->getSetting('max_length')) {
+      $constraint_manager = \Drupal::typedDataManager()->getValidationConstraintManager();
+      $constraints[] = $constraint_manager->create('ComplexData', [
+        'value' => [
+          'Length' => [
+            'max' => $max_length,
+            'maxMessage' => t('%name: may not be longer than @max characters.', [
+              '%name' => $this->getFieldDefinition()->getLabel(),
+              '@max' => $max_length
+            ]),
+          ],
+        ],
+      ]);
+    }
+
+    return $constraints;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function generateSampleValue(FieldDefinitionInterface $field_definition) {
+    $random = new Random();
+    $values['value'] = $random->word(mt_rand(1, $field_definition->getSetting('max_length')));
+    return $values;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fieldSettingsForm(array $form, FormStateInterface $form_state) {
+    return [
+      'prefix' => [
+        '#type' => 'textfield',
+        '#title' => $this->t('Prefix'),
+        '#description' => $this->t( 'Comma-delimited list of base subjects to limit the search to.'),
+      ],
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function storageSettingsForm(array &$form, FormStateInterface $form_state, $has_data) {
+    $elements = [];
+
+    $elements['max_length'] = [
+      '#type' => 'number',
+      '#title' => t('Maximum length'),
+      '#default_value' => $this->getSetting('max_length'),
+      '#required' => TRUE,
+      '#description' => t('The maximum length of the field in characters.'),
+      '#min' => 1,
+      '#disabled' => $has_data,
+    ];
+
+    return $elements;
+  }
+
+  public static function mainPropertyName() {
+    return 'value';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function isEmpty() {
+    $value = $this->get('value')->getValue();
+    return $value === NULL || $value === '';
+  }
+
+}
