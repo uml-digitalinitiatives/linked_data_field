05fec838d2ac7f48598bf827b67171d81394c0ca
Alexander O'Neill <aoneill@upei.ca>
2019-04-24 16:25:36 -0300

    Add some more testing code.

--- src/LCLookupService.php
+++ src/LCLookupService.php
@@ -35,11 +35,24 @@ class LCLookupService implements LCLookupServiceInterface {
     // Get base URL from config.
     $config = $this->configFactory->get('lc_subject_field.settings');
     $base_url = $config->get('base_url');
+    // For automated testing.
+    if ((string) $base_url == 'http://test.test/') {
+      return $this->testData($candidate);
+    }
     $request = $this->httpClient->get($base_url . 'authorities/subjects/suggest/?q=' .
     urlencode($candidate));
     $response = json_decode($request->getBody());
     return array_combine($response[1], $response[3]);
   }
 
-
+  protected function testData($candidate) {
+    if ($candidate[0] == 'a') {
+      return ['Alien Abduction' => 'http://nasa.gov/',
+        'Apple, Inc.' => 'http://apple.com/',];
+    }
+    else {
+      return ['borax' => 'http://bbc.co.uk/',
+        'Bat' => 'http://bat.cave'];
+    }
+  }
 }
--- tests/src/FunctionalJavascript/LoadTest.php
+++ tests/src/FunctionalJavascript/LoadTest.php
@@ -1,11 +1,10 @@
 <?php
 
-namespace Drupal\Tests\lc_subject_field\Functional;
+namespace Drupal\Tests\lc_subject_field\FunctionalJavascript;
 
 use Drupal\Core\Url;
-use Drupal\field_ui\Tests\FieldUiTestTrait;
+
 use Drupal\FunctionalJavascriptTests\WebDriverTestBase;
-use Drupal\Tests\lc_subject_field\FunctionalJavascript\LoginAdminTrait;
 
 
 /**
@@ -16,7 +15,6 @@ use Drupal\Tests\lc_subject_field\FunctionalJavascript\LoginAdminTrait;
 class LoadTest extends WebDriverTestBase {
 
   use LoginAdminTrait;
-  use FieldUiTestTrait;
 
   /**
    * Modules to enable.
@@ -71,8 +69,88 @@ class LoadTest extends WebDriverTestBase {
       'cardinality' => '-1',
     ], []);
 
-    $this->assertSession()->pageTextContains('');
+//    $this->assertSession()->pageTextContains('');
+    // Change the base URL to not hit id.loc.gov.
+    $this->config('lc_subject_field.settings')->set('base_url', 'http://test.test/')->save();
+    $this->drupalGet('node/add/article');
+    $this->getSession()->getPage()->fillField('Subject', 'a');
+    $this->triggerEventOn('input[name="field_subject[0][value]"]');
+  }
 
+
+  /**
+   * Creates a new field through the Field UI.
+   * Borrowed from field_ui module tests.
+   *
+   * @param string $bundle_path
+   *   Admin path of the bundle that the new field is to be attached to.
+   * @param string $field_name
+   *   The field name of the new field storage.
+   * @param string $label
+   *   (optional) The label of the new field. Defaults to a random string.
+   * @param string $field_type
+   *   (optional) The field type of the new field storage. Defaults to
+   *   'test_field'.
+   * @param array $storage_edit
+   *   (optional) $edit parameter for drupalPostForm() on the second step
+   *   ('Storage settings' form).
+   * @param array $field_edit
+   *   (optional) $edit parameter for drupalPostForm() on the third step ('Field
+   *   settings' form).
+   */
+  public function fieldUIAddNewField($bundle_path, $field_name, $label = NULL, $field_type = 'test_field', array $storage_edit = [], array $field_edit = []) {
+    $label = $label ?: $this->randomString();
+    $initial_edit = [
+      'new_storage_type' => $field_type,
+      'label' => $label,
+    ];
+
+    // Allow the caller to set a NULL path in case they navigated to the right
+    // page before calling this method.
+    if ($bundle_path !== NULL) {
+      $bundle_path = "$bundle_path/fields/add-field";
+    }
+
+    // First step: 'Add field' page.
+    $this->drupalGet($bundle_path);
+
+
+    $this->submitForm($initial_edit, "Save and continue");
+    $initial_edit['field_name'] = 'subject';
+    $this->submitForm($initial_edit, "Save and continue");
+
+    //return $this->getSession()->getPage()->getContent();
+
+    //$this->drupalPostForm($bundle_path, $initial_edit, t('Save and continue'));
+    $this->assertRaw((string) t('These settings apply to the %label field everywhere it is used.', ['%label' => $label]));
+
+    // Second step: 'Storage settings' form.
+    $this->drupalPostForm(NULL, $storage_edit, t('Save field settings'));
+    $this->assertRaw((string) t('Updated field %label field settings.', ['%label' => $label]), 'Redirected to field settings page.');
+
+    // Third step: 'Field settings' form.
+    $this->drupalPostForm(NULL, $field_edit, t('Save settings'));
+    $this->assertRaw((string) t('Saved %label configuration.', ['%label' => $label]));
+
+    // Check that the field appears in the overview form.
+    $this->assertFieldByXPath('//table[@id="field-overview"]//tr/td[1]', $label);
+
+  }
+
+  public function triggerEventOn($css_selector){
+    $function =
+        '(function(){'
+        .'var element = document.querySelector("$css_selector");'
+        . 'var event = document.createEvent("Event");'
+        . 'event.initEvent('change', true, true);'
+        . 'element.dispatchEvent(event);'
+        . '})();';
+
+    try{
+      $this->getSession()->getDriver()->executeScript($function);
+    }catch (\Exception $e){
+
+    }
   }
 
 }
