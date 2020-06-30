fc4ecdb58481f5d44b45510962cee1c207659cfd
Alexander O'Neill <aloneill@gmail.com>
2020-06-29 23:12:29 -0300

    Issue #3155210 by alxp: Fix broken JS selector on Claro theme.

--- js/subject_autocomplete.js
+++ js/subject_autocomplete.js
@@ -1,7 +1,8 @@
 Drupal.autocomplete.options.select =  function selectHandler(event, ui) {
-  var subjectURLField = event.target.parentNode.parentNode.getElementsByClassName('subject-url-input')[0];
-  if (subjectURLField !== undefined) {
-    subjectURLField.value = ui.item.value;
+  var urlFieldName = event.target.name.replace("[value]", "[url]");
+  var urlField = document.getElementsByName(urlFieldName)[0];
+  if (urlField !== undefined) {
+    urlField.value = ui.item.value;
     event.target.value = ui.item.label;
     // Hack. Without this the form elements added via AJAX don't get saved.
     event.target.setAttribute('value', ui.item.label);
